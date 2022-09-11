41c2ce5349b30e6d78c361e4f4c254dea35c8def
maneeshsharma404 <maneeshsharma881@gmail.com>
2022-09-12 00:54:31 +0530

    database function

--- src/main/java/com/helper/Database.java
+++ src/main/java/com/helper/Database.java
@@ -1,5 +1,6 @@
 package com.helper;
 
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,21 +9,19 @@ import java.sql.Statement;
 public class Database {
 	
 	public static void main(String[] args) {
-		try{  
-			System.out.println("in try db block start"); 
-			/*
-			 * //here sonoo is database name, root is username and password Statement
-			 * stmt=con.createStatement(); ResultSet rs=stmt.
-			 * executeQuery("insert into users ('id', 'name', 'adhar', 'email', 'password') values('\" + 20 + \"','\" + maneesh\r\n"
-			 * +
-			 * "                + \"','\" + 821565 + \"','\" + maneesh22 + \"','\" + l + \"');"
-			 * ); while(rs.next())
-			 * System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
-			 * System.out.println("in try db block end");
-			 */
-			
-			}catch(Exception e){ System.out.println(e);} 
+		
 	}
- 
+	
+	public String databasestring() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection obj=DriverManager.getConnection("jdbc:mysql://localhost/votersystem","root","admin@123");
+			String  con = obj;
+			return con;
+	
+			}
+		catch(Exception e){ System.out.println(e);} 
+		
+	}
 	  
 }
